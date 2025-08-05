e3e24f2fde033a13498503af32bd8896ff3f43f9
godisdeadLOL <hidden>
2025-08-05 22:40:03 +0300

    my best photo + drip

---
+++ src/components/DoubleSidedImage.tsx
@@ -0,0 +1,19 @@
+import type { HTMLAttributes } from "preact/compat"
+
+type DoubleSidedImageProps = {
+	frontSrc: string
+	backSrc: string
+
+	className?: string
+} & HTMLAttributes<HTMLDivElement>
+
+export function DoubleSidedImage({ frontSrc, backSrc, className, ...other }: DoubleSidedImageProps) {
+	const imgClass = "block transition-transform duration-500 backface-hidden pointer-events-none select-none"
+
+	return (
+		<div tabIndex={1} className={`group relative perspective-distant bg-background-elevate ${className ?? ""}`} {...other}>
+			<img src={frontSrc} className={`${imgClass} rotate-y-[0deg] group-focus:rotate-y-[180deg] w-full `} />
+			<img src={backSrc} className={`${imgClass} rotate-y-[180deg] group-focus:rotate-y-[360deg] absolute left-0 top-0 w-full h-full`} />
+		</div>
+	)
+}
--- src/page/About.tsx
+++ src/page/About.tsx
@@ -1,3 +1,4 @@
+import { DoubleSidedImage } from "@/components/DoubleSidedImage"
 import { Header } from "@/components/Header"
 
 export const About = () => {
@@ -6,12 +7,14 @@ export const About = () => {
 			<Header id="about">обо мне</Header>
 
 			<div class="text-justify [&_>_*]:mb-8">
+				<p>Я Гордеев Владислав, мне 23 года.</p>
+
 				<p>Увлекаюсь геймдевом и имею опыт разработки игр на разных движках, преимущественно на Unity.</p>
 
 				<p>В настоящее время изучаю React, реализовал несколько небольших личных проектов и продолжаю активно развиваться в этой области.</p>
 			</div>
 
-			<img src="funny.jpg" className="max-w-xs mx-auto rounded-md" />
+			<DoubleSidedImage frontSrc="mybestphoto.png" backSrc="drip.jpg" className="max-w-xs mx-auto rounded-md [&_img]:rounded-md" />
 		</section>
 	)
 }
